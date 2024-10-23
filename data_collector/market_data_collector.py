83988f79dca4610db05ab78db4ec5313f9007432
sadeeshakgr <161478625+sadeekgr@users.noreply.github.com>
2024-10-24 00:27:43 +0200

    secret file access in market_data_collector.py

--- data_collector/market_data_collector.py
+++ data_collector/market_data_collector.py
@@ -1,5 +1,8 @@
+import secret_codes
 from .data_collector import DataCollector
-
+from secret_codes import secret_codes
+API_KEY = secret_codes["Alpaca API Key"]
+API_SECRET_KEY = secret_codes["Alpaca API Secret Key"]
 
 class MarketDataCollector(DataCollector):
     def __init__(self):
