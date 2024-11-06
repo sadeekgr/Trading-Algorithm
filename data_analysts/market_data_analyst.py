0f22270ab8ac8b0a3d0401de76433a1fb0574ab6
sadeeshakgr <161478625+sadeekgr@users.noreply.github.com>
2024-11-06 20:04:08 +0100

    MarketDataAnalyst support and resistance implemented

--- data_analysts/market_data_analyst.py
+++ data_analysts/market_data_analyst.py
@@ -21,14 +21,15 @@ class MarketDataAnalyst(DataAnalyst):
             'ROC': [12], # rate of change, seems to behave like a pendulum (oscillator) with resistance, oscillates between similar highs and lows (or little less than earlier ones), especially when there is a bit of volatility
             'ROC_of_ROC': [12],
             'mean_absolute_deviation_from_linear_ROC': [12], # mean absolute deviation of ROC compared to its linear regression
-            #'support': [3, 59], # strenght proportional to volume
-            #'resistance': [3, 59], # strenght proportional to volume
+            'support': [43, 177, 599], # strenght proportional to volume and difference between close and low, should divide price in bands whose width is 0.1% of curr price
+            'resistance': [43, 177, 599], # strenght proportional to volume and difference between high and close, should divide price in bands whose width is 0.1% of curr price
+            # inclined support and resistance are caused by market orders (they are a 'perceived' resistance and support), horizontal are caused by limit orders (they usually represent 'actual' support and resistance, should be stronger as they might be bulk orders from companies)
             # maybe consider with what angle the EMA intersects the RMAs
             # average up / down movement's duration and speed calculated through ROC
             # consider change in num of shares (use an average share count?)
             # order book / market depth / liquidity heatmap
             # Bollinger Bands
-            #'RSI': [14],
+            # 'RSI': [14],
             # pre and after market price action
             # analyze time of day (sleep, breakfast, after work)
         }
@@ -63,28 +64,156 @@ class MarketDataAnalyst(DataAnalyst):
         elif indicator == 'ROC':
             indicator_data = ta.ROC(prices, timeperiod=timeperiod)
         elif indicator == 'ROC_of_ROC':
-            roc_values = ta.ROC(prices, timeperiod=timeperiod)
-            first_valid_value = roc_values[~np.isnan(roc_values)][0]
-            roc_values = np.nan_to_num(roc_values, nan=first_valid_value)
-            indicator_data = ta.ROC(roc_values, timeperiod=timeperiod)
+            indicator_data = self.ROC_of_ROC(prices, timeperiod=timeperiod)
         elif indicator == 'mean_absolute_deviation_from_linear_ROC':
-            roc_values = ta.ROC(prices, timeperiod=timeperiod)
-            first_valid_value = roc_values[~np.isnan(roc_values)][0]
-            roc_values = np.nan_to_num(roc_values, nan=first_valid_value)
-            x = np.arange(len(roc_values)).reshape(-1, 1)
-            linear_model = LinearRegression()
-            linear_model.fit(x, roc_values)
-            linear_values = linear_model.predict(x)
-            absolute_deviations = np.abs(roc_values - linear_values)
-            absolute_deviation_series = pd.Series(absolute_deviations)
-            indicator_data = absolute_deviation_series.rolling(window=timeperiod).mean().to_numpy()
+            indicator_data = self.mean_absolute_deviation_from_linear_ROC(prices, timeperiod=timeperiod)
         elif indicator == 'support':
-            pass
+            indicator_data = self.support(prices, timeperiod=timeperiod)
         elif indicator == 'resistance':
-            pass
+            indicator_data = self.resistance(prices, timeperiod=timeperiod)
         first_valid_value = indicator_data[~np.isnan(indicator_data)][0]
         indicator_data = np.nan_to_num(indicator_data, nan=first_valid_value)
         return indicator_data
+
+    def support(self, prices, timeperiod, use_candles=True, use_volume=True, use_smoothing=False): # TO UPDATE smoothing doesn't work well, maybe use numpy arrays to make calculations faster, maybe consider groups of candles not just single low or high
+        rolling_lows = ta.MIN(prices['low'], timeperiod=timeperiod)
+        rolling_lows = rolling_lows[rolling_lows != rolling_lows.shift(1)].dropna()
+        lows = pd.DataFrame(columns=prices.columns)
+        for timestamp, low_value in rolling_lows.items():
+            current_position = prices.index.get_loc(timestamp)
+            start_position = max(0, current_position - timeperiod + 1)
+            earlier_prices = prices.iloc[start_position:current_position+1]
+            match = earlier_prices[earlier_prices['low'] == low_value]
+            most_recent_row = match.iloc[[0]]
+            lows = pd.concat([lows, most_recent_row])
+        volume = 1
+        if use_volume:
+            volume = lows['volume']
+        close_low_diff = high_low_diff = 1
+        high_close_diff = 0
+        if use_candles:
+            close_low_diff = lows['close'] - lows['low']
+            high_close_diff = lows['high'] - lows['close']
+            high_low_diff = lows['high'] - lows['low']
+        lows['support_strength'] = ((close_low_diff * volume) - (high_close_diff * volume)) / high_low_diff
+        lows.fillna(0, inplace=True)
+        indicator_data = pd.DataFrame(index=prices.index)
+        indicator_data['support_strength'] = 0.0
+        indicator_data.loc[lows.index, 'support_strength'] = lows['support_strength']
+        indicator_data['price'] = pd.NA
+        indicator_data.loc[lows.index, 'price'] = lows['low']
+        indicator_data['support_strength'] = indicator_data['support_strength'].rolling(window=timeperiod, min_periods=1).max()
+        indicator_data['support_strength'] = indicator_data['support_strength'].where(indicator_data['support_strength'] > 0, pd.NA)
+        indicator_data.ffill(inplace=True)
+        indicator_data.bfill(inplace=True)
+        indicator_data.reset_index(inplace=True)
+        for i in range(1, len(indicator_data)):
+            if indicator_data['support_strength'].iloc[i] == indicator_data['support_strength'].iloc[i - 1]:
+                indicator_data.loc[indicator_data.index[i], 'price'] = indicator_data['price'].iloc[i - 1]
+            else:
+                strength = indicator_data['support_strength'].iloc[i]
+                matching_lows = lows[(lows['support_strength'] == strength) & (lows.index <= indicator_data['timestamp'].iloc[i])]['low']
+                if not matching_lows.empty:
+                    indicator_data.loc[indicator_data.index[i], 'price'] = matching_lows.values[-1]
+                else:
+                    indicator_data.loc[indicator_data.index[i], 'price'] = pd.NA
+        if use_smoothing:
+            support = np.empty(len(indicator_data))
+            for i in range(len(indicator_data)):
+                if i < timeperiod:
+                    window_len = i + 1
+                    if window_len == 1:
+                        support[i] = indicator_data.iloc[:i + 1].reset_index(drop=True)['price'][0]
+                        continue
+                else:
+                    window_len = timeperiod
+                current_window = indicator_data.iloc[i - window_len + 1:i + 1].reset_index(drop=True)
+                current_window['change'] = current_window['support_strength'] != current_window['support_strength'].shift(1)
+                current_window_prices = current_window[current_window['change']]['price'].values
+                current_window_weights = current_window[current_window['change']]['support_strength'].values
+                current_window_pos = current_window[current_window['change']].index.values
+                X = current_window_pos.reshape(-1, 1)
+                y = current_window_prices
+                model = LinearRegression()
+                model.fit(X, y, sample_weight=current_window_weights)
+                full_range = np.arange(0, window_len).reshape(-1, 1)
+                predicted_prices = model.predict(full_range)
+                weighted_mean_price = float(np.sum(current_window_prices * current_window_weights) / sum(current_window_weights))
+                predicted_prices = (predicted_prices + weighted_mean_price) / 2
+                support[i - window_len + 1:i + 1] = predicted_prices
+        else:
+            support = indicator_data['price'].to_numpy()
+        return support
+
+    def resistance(self, prices, timeperiod, use_candles=True, use_volume=True, use_smoothing=False): # TO UPDATE, smoothing doesn't work well, maybe use numpy arrays to make calculations faster, maybe consider groups of candles not just single low or high
+        rolling_highs = ta.MAX(prices['high'], timeperiod=timeperiod)
+        rolling_highs = rolling_highs[rolling_highs != rolling_highs.shift(1)].dropna()
+        highs = pd.DataFrame(columns=prices.columns)
+        for timestamp, high_value in rolling_highs.items():
+            current_position = prices.index.get_loc(timestamp)
+            start_position = max(0, current_position - timeperiod + 1)
+            earlier_prices = prices.iloc[start_position:current_position + 1]
+            match = earlier_prices[earlier_prices['high'] == high_value]
+            most_recent_row = match.iloc[[0]]
+            highs = pd.concat([highs, most_recent_row])
+        volume = 1
+        if use_volume:
+            volume = highs['volume']
+        high_close_diff = high_low_diff = 1
+        close_low_diff = 0
+        if use_candles:
+            high_close_diff = highs['high'] - highs['close']
+            close_low_diff = highs['close'] - highs['low']
+            high_low_diff = highs['high'] - highs['low']
+        highs['resistance_strength'] = ((high_close_diff * volume) - (close_low_diff * volume)) / high_low_diff
+        highs.fillna(0, inplace=True)
+        indicator_data = pd.DataFrame(index=prices.index)
+        indicator_data['resistance_strength'] = 0.0
+        indicator_data.loc[highs.index, 'resistance_strength'] = highs['resistance_strength']
+        indicator_data['price'] = pd.NA
+        indicator_data.loc[highs.index, 'price'] = highs['high']
+        indicator_data['resistance_strength'] = indicator_data['resistance_strength'].rolling(window=timeperiod, min_periods=1).max()
+        indicator_data['resistance_strength'] = indicator_data['resistance_strength'].where(indicator_data['resistance_strength'] > 0, pd.NA)
+        indicator_data.ffill(inplace=True)
+        indicator_data.bfill(inplace=True)
+        indicator_data.reset_index(inplace=True)
+        for i in range(1, len(indicator_data)):
+            if indicator_data['resistance_strength'].iloc[i] == indicator_data['resistance_strength'].iloc[i - 1]:
+                indicator_data.loc[indicator_data.index[i], 'price'] = indicator_data['price'].iloc[i - 1]
+            else:
+                strength = indicator_data['resistance_strength'].iloc[i]
+                matching_lows = highs[(highs['resistance_strength'] == strength) & (highs.index <= indicator_data['timestamp'].iloc[i])]['high']
+                if not matching_lows.empty:
+                    indicator_data.loc[indicator_data.index[i], 'price'] = matching_lows.values[-1]
+                else:
+                    indicator_data.loc[indicator_data.index[i], 'price'] = pd.NA
+        if use_smoothing:
+            resistance = np.empty(len(indicator_data))
+            for i in range(len(indicator_data)):
+                if i < timeperiod:
+                    window_len = i + 1
+                    if window_len == 1:
+                        resistance[i] = indicator_data.iloc[:i + 1].reset_index(drop=True)['price'][0]
+                        continue
+                else:
+                    window_len = timeperiod
+                current_window = indicator_data.iloc[i - window_len + 1:i + 1].reset_index(drop=True)
+                current_window['change'] = current_window['resistance_strength'] != current_window['resistance_strength'].shift(1)
+                current_window_prices = current_window[current_window['change']]['price'].values
+                current_window_weights = current_window[current_window['change']]['resistance_strength'].values
+                current_window_pos = current_window[current_window['change']].index.values
+                X = current_window_pos.reshape(-1, 1)
+                y = current_window_prices
+                model = LinearRegression()
+                model.fit(X, y, sample_weight=current_window_weights)
+                full_range = np.arange(0, window_len).reshape(-1, 1)
+                predicted_prices = model.predict(full_range)
+                weighted_mean_price = float(np.sum(current_window_prices * current_window_weights) / sum(current_window_weights))
+                predicted_prices = (predicted_prices + weighted_mean_price) / 2
+                resistance[i - window_len + 1:i + 1] = predicted_prices
+        else:
+            resistance = indicator_data['price'].to_numpy()
+        return resistance
 
     def RMA(self, prices, timeperiod):
         alpha = 1 / timeperiod
@@ -94,13 +223,30 @@ class MarketDataAnalyst(DataAnalyst):
             rma[i] = (1 - alpha) * rma[i - 1] + alpha * prices[i]
         return rma
 
-    def analyze_data(self):
-        pass
+    def ROC_of_ROC(self, prices, timeperiod):
+        roc_values = ta.ROC(prices, timeperiod=timeperiod)
+        first_valid_value = roc_values[~np.isnan(roc_values)][0]
+        roc_values = np.nan_to_num(roc_values, nan=first_valid_value)
+        roc_of_roc = ta.ROC(roc_values, timeperiod=timeperiod)
+        return roc_of_roc
+
+    def mean_absolute_deviation_from_linear_ROC(self, prices, timeperiod):
+        roc_values = ta.ROC(prices, timeperiod=timeperiod)
+        first_valid_value = roc_values[~np.isnan(roc_values)][0]
+        roc_values = np.nan_to_num(roc_values, nan=first_valid_value)
+        x = np.arange(len(roc_values)).reshape(-1, 1)
+        linear_model = LinearRegression()
+        linear_model.fit(x, roc_values)
+        linear_values = linear_model.predict(x)
+        absolute_deviations = np.abs(roc_values - linear_values)
+        absolute_deviation_series = pd.Series(absolute_deviations)
+        mean_absolute_deviation_from_linear_roc = absolute_deviation_series.rolling(window=timeperiod).mean().to_numpy()
+        return mean_absolute_deviation_from_linear_roc
 
 
 if __name__ == '__main__':
     mk = MarketDataAnalyst()
-    data = mk.get_indicators(f'../sprt.csv')
+    data = mk.get_indicators(f'../data/market_data/historical_market_data/Start_2024-10-24-00-00-00_End_2024-10-28-00-00-00_Interval_1Min/NVDA_NASDAQ_historical_market_data.csv')
     data.reset_index(inplace=True)
     fin.background = '#000000'
     fin.odd_plot_background = '#000000'
@@ -109,14 +255,25 @@ if __name__ == '__main__':
     fin.cross_hair_color = '#ffffff'
     fin.legend_text_color = '#ffffff'
     fin.scale_text_color = '#ffffff'
-    ax, ax2, ax3 = fin.create_plot('AAPL', rows=3)
+    ax, ax2, axv = fin.create_plot('AAPL', rows=3)
     candles = data[['timestamp', 'open', 'close', 'high', 'low']]
     fin.candlestick_ochl(candles, ax=ax)
-    fin.plot(data['timestamp'], data['EMA_13'], ax=ax, color='#486c77', legend='EMA 13')
-    fin.plot(data['timestamp'], data['RMA_21'], ax=ax, color='#7297c4', legend='RMA 21')
-    fin.plot(data['timestamp'], data['RMA_53'], ax=ax, color='#2a632e', legend='RMA 53')
-    fin.plot(data['timestamp'], data['RMA_199'], ax=ax, color='#83222b', legend='RMA 199')
+    volumes = data[['timestamp', 'open', 'close', 'volume']]
+    fin.volume_ocv(volumes, ax=axv)
+    #fin.plot(data['timestamp'], data['EMA_13'], ax=ax, color='#486c77', legend='EMA 13')
+    #fin.plot(data['timestamp'], data['RMA_21'], ax=ax, color='#7297c4', legend='RMA 21')
+    #fin.plot(data['timestamp'], data['RMA_53'], ax=ax, color='#2a632e', legend='RMA 53')
+    #fin.plot(data['timestamp'], data['RMA_199'], ax=ax, color='#83222b', legend='RMA 199')
+    #fin.plot(data['timestamp'], data['LOW_7'], ax=ax, legend='LOW 7')
+    #fin.plot(data['timestamp'], data['LOW_43'], ax=ax, legend='LOW 43')
+    #fin.plot(data['timestamp'], data['LOW_177'], ax=ax, legend='LOW 177')
+    fin.plot(data['timestamp'], data['support_43'], ax=ax, legend='support 43')
+    fin.plot(data['timestamp'], data['support_177'], ax=ax, legend='support 177')
+    fin.plot(data['timestamp'], data['support_599'], ax=ax, legend='support 599')
+    fin.plot(data['timestamp'], data['resistance_43'], ax=ax, legend='resistance 43')
+    fin.plot(data['timestamp'], data['resistance_177'], ax=ax, legend='resistance 177')
+    fin.plot(data['timestamp'], data['resistance_599'], ax=ax, legend='resistance 599')
     fin.plot(data['timestamp'], data['ROC_12'], ax=ax2, legend='ROC 12')
     fin.plot(data['timestamp'], data['mean_absolute_deviation_from_linear_ROC_12'], ax=ax2, legend='MAD from Linear ROC 12')
-    fin.plot(data['timestamp'], data['ROC_of_ROC_12'], ax=ax3, legend='ROC of ROC 12')
+    #fin.plot(data['timestamp'], data['ROC_of_ROC_12'], ax=ax3, legend='ROC of ROC 12')
     fin.show()
--- test.py
+++ test.py
@@ -1,39 +1,53 @@
 from datetime import datetime, timedelta
-import time, re, requests, json, os
+import time, re, requests, json, os, talib
 import pandas as pd
 from bs4 import BeautifulSoup
 from finvizfinance.quote import Statements, finvizfinance
 from alpaca.data import TimeFrame, StockHistoricalDataClient, StockLatestTradeRequest, StockLatestQuoteRequest, StockBarsRequest
 from secret_codes import secret_codes
 from tvDatafeed import TvDatafeed, Interval
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 API_KEY = secret_codes["Alpaca API Key"]
 API_SECRET_KEY = secret_codes["Alpaca API Secret Key"]
 tv_username = secret_codes["TradingView username"]
 tv_password = secret_codes["TradingView password"]
 
-import talib
-print(dir(talib))
-o = talib.EMA()
-symbol = 'SOC.WS'
-exchange = 'NYSE'
-stock = finvizfinance("AAPL")
-data = stock.ticker_full_info()
-print(data)
-start = datetime(year=2022, month=10, day=24, hour=0, minute=0, second=0)
-end = datetime(year=2023, month=10, day=19, hour=0, minute=0, second=0)
-num_bars_needed = 5
-client = StockHistoricalDataClient(API_KEY, API_SECRET_KEY)
-request_params = StockBarsRequest(
-    symbol_or_symbols=symbol,
-    timeframe=TimeFrame.Minute,
-    start=start,
-    end=end
-)
-historical_market_data_alpaca = client.get_stock_bars(request_params).df
-tv = TvDatafeed(tv_username, tv_password)
-tv_data = tv.get_hist(symbol, exchange, interval=Interval.in_1_minute, n_bars=num_bars_needed, extended_session=True)
-d = tv_data.index[0]
-filtered_df = tv_data[tv_data.index >= start]
-interval = Interval.in_1_minute
-filename = "market_data.csv"
-print(filtered_df)
+
+# Original data
+data = {
+    'timestamp': ['2023-01-01', '2023-01-02', '2023-01-04', '2023-01-05'],
+    'price': [100, 102, 104, 105],
+    'weight': [1, 2, 1, 3]  # Example weights
+}
+df = pd.DataFrame(data)
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+# Calculate weighted mean price
+mean_price = np.average(df['price'], weights=df['weight'])
+num_samples = sum(data['weight'])
+
+# Prepare data for regression
+X = np.array(range(len(df))).reshape(-1, 1)  # Use indices as independent variable
+y = df['price'].values  # Prices as dependent variable
+weights = df['weight'].values
+
+# Fit weighted linear regression model
+model = LinearRegression()
+model.fit(X, y, sample_weight=weights)
+df['predicted_price'] = model.predict(X)
+
+# Plotting
+plt.figure(figsize=(10, 5))
+plt.scatter(df['timestamp'], df['price'], label='Actual Prices', color='blue', alpha=0.6)
+plt.plot(df['timestamp'], df['predicted_price'], label='Weighted Regression Line', color='red', linewidth=2)
+plt.axhline(mean_price, color='green', linestyle='--', label='Mean Price')
+plt.title('Price Over Time with Weighted Linear Regression Line, Mean Price, and Samples')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
