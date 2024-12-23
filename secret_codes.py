68872f9b1a5638dc2eb9dc59b0f7e7b70d8ca6c1
Sadeesha Ranasinghe <161478625+sadeekgr@users.noreply.github.com>
2024-12-23 21:47:06 +0100

    secret_codes.py

---
+++ secret_codes.py
@@ -0,0 +1,7 @@
+secret_codes = {
+    "Alpaca code to bypass 2FA": "XXXX",
+    "Alpaca API Key": "XXXX",
+    "Alpaca API Secret Key": "XXXX",
+    "TradingView username": "XXXX",
+    "TradingView password": "XXXX"
+}
